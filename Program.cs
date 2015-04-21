7496ca7cb3874535f1538f450ab41e3be240761d
amoshag <amos@optibus.co>
2015-04-21 11:47:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using MightyLittleGeodesy.Positions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace RT90ToWGS84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coordinates = new List<string>();
+
+            foreach (string line in File.ReadAllLines("input.csv"))
+            {
+                string[] tokens = line.Split(',');
+                double x = double.Parse(tokens[0]);
+                double y = double.Parse(tokens[1]);
+                RT90Position position = new RT90Position(x, y);
+                WGS84Position wgsPos = position.ToWGS84();
+                coordinates.Add(string.Format("{0},{1}", wgsPos.Longitude, wgsPos.Latitude));
+            }
+
+            File.WriteAllLines("output.csv", coordinates);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RT90ToWGS84
-Converts RT90 coordinates (Sweden) to WDG84
+Simple command line tool to convert RT90 coordinates (Sweden) to WGS84, using the code in http://blog.sallarp.com/translate-coordinates-between-rt90-wgs84-and-sweref99-using-net/
+The program reads a file named input.csv from the running directory, the file should be in csv format (comma separated) with two columns, the first one is the X coordinate of RT90 and the second is the Y coordinate.
+The program output a file named output.csv with two columns, the first one is the Longitudue and the second is the Latitude (WGS84).
